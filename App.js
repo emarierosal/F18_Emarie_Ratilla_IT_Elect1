2c3f13f001afb3e8278f574d0b371af7fe4243b7
emarierosal <emarierosalratilla@gmail.com>
2025-09-25 09:22:51 +0800

    Activity5

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return ( <
+        SafeAreaView style = {
+            { flex: 1 }
+        } >
+        <
+        CounterApp / >
+        <
+        ColorChangerApp / >
+        <
+        /SafeAreaView>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
